256e73dc19d429372927c42bf4d0ffde536eb0b1
Jacob <aguilat595@gmail.com>
2021-07-02 14:17:42 -0600

    Add files react proyect

---
+++ src/CommerceApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CommerceApp = () => {
+    return (
+        <div>
+            <h1>Hola mundo</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CommerceApp } from './CommerceApp'
+
+
+ReactDOM.render(
+    <CommerceApp />,
+  document.getElementById('root')
+);
+
